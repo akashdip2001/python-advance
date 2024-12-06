473823fd073a18de625dc260834d86ea64ec36a1
akashdip2001 <akashdip.mahapatra.21@aot.edu.in>
2024-12-07 02:30:00 +0530

    update Day 2.2 api readme

--- Py for DevOps/Day 2.2 - API test/api_test.py
+++ Py for DevOps/Day 2.2 - API test/api_test.py
@@ -1,15 +1,28 @@
-# As a DevOps Engineer, Your Developer has an API (Application Programming Interface) that you need to test.
+# # As a DevOps Engineer, Your Developer has an API (Application Programming Interface) that you need to test.
+
+# import requests
+# # pip install requests ## install in my Global Python Environment in PC
+
+# demo_url = "https://jsonplaceholder.typicode.com/posts"
+
+# response = requests.get(url=demo_url)
+# print(response)
+# #output: <Response [200]> ## 200 means the request was successful
+
+# # print(response.json()) # get list of dictionaries [{}, {}, {}]
+# # print(type(response.json())) # <class 'list'>
+
+# print(response.json()[0]) # get first dictionary {}
+
+
 
 import requests
-# pip install requests ## install in my Global Python Environment in PC
 
-demo_url = "https://jsonplaceholder.typicode.com/posts"
+def test_api():
+    demo_url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url=demo_url)
+    print(response)
+    return response.json()[0]
 
-response = requests.get(url=demo_url)
-print(response)
-#output: <Response [200]> ## 200 means the request was successful
-
-# print(response.json()) # get list of dictionaries [{}, {}, {}]
-# print(type(response.json())) # <class 'list'>
-
-print(response.json()[0]) # get first dictionary {}
+X = test_api() # call the function to test the API
+print(X)
--- Py for DevOps/Day 2.2 - API test/readme.md
+++ Py for DevOps/Day 2.2 - API test/readme.md
@@ -15,8 +15,31 @@ print(response)
 #output: <Response [200]> ## 200 means the request was successful
 ```
 
-#### `print(response.json())` # get list of dictionaries [{}, {}, {}]
-#### `print(type(response.json()))` # <class 'list'>
+#### `print(response.json())` get list of dictionaries [{}, {}, {}]
+#### `print(type(response.json()))` <class 'list'>
+
 ```python
 print(response.json()[0]) # get first dictionary {}
+```
+
+# Rap all in a function
+
+```python
+import requests
+
+def test_api():
+    demo_url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url=demo_url)
+    print(response)
+    return response.json()[0]
+
+X = test_api() # call the function to test the API
+print(X)
+```
+Output:
+```bash
+PS C:\Users\akash\Desktop\Py Projects\python-advance\Py for DevOps\Day 2.2 - API test> python .\api_test.py
+<Response [200]>
+{'userId': 1, 'id': 1, 'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', 'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}
+PS C:\Users\akash\Desktop\Py Projects\python-advance\Py for DevOps\Day 2.2 - API test> 
 ```
